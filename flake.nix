78193d0c6f6b364c4b5f8eb4d97d13810894d51a
Torsten Schmits <torstenschmits@gmail.com>
2021-06-16 14:45:49 +0200

    bazel: add tests for lib_crypto

--- flake.nix
+++ flake.nix
@@ -11,11 +11,17 @@
       p.libffi
     ];
 
+    # NOTE: the package `bls12-381-unix` has a dependency on a rust library that fails to build with nix due to
+    # interpreter paths being patched, resulting in an sha mismatch.
+    # To fix this, the patch script `.opam/opam-init/hooks/shebangs.sh` must be suspended by commenting out its last
+    # line when installing that dependency.
     depsOpam = [
+      "alcotest-lwt"
       "angstrom"
       "async"
       "base64"
       "bls12-381"
+      "bls12-381-unix"
       "ca-certs"
       "fmt"
       "ipaddr"
